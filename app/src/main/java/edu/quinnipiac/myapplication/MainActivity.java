d02b04ee5c93566d9610ee37a55f11beffa410d4
Dominic Smorra <dominicsmorra@gmail.com>
2019-03-19 13:47:54 -0400

    message

---
+++ app/src/main/java/edu/quinnipiac/myapplication/MainActivity.java
@@ -0,0 +1,40 @@
+package edu.quinnipiac.myapplication;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void itemClicked(Long id){
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if(fragmentContainer != null) {
+
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            ft.replace(R.id.fragment_container, details);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.addToBackStack(null);
+            ft.commit();
+
+
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
+
+    }
+
+
+}
